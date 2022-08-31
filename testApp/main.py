360d124990a21211729a37806a64644bf8a94d6d
realhardik18 <realhardik18@gmail.com>
2022-08-31 12:23:08 +0530

    got auth working with my own account

---
+++ testApp/main.py
@@ -0,0 +1,14 @@
+import os
+from Google import Create_Service
+import pandas as pd
+
+API_NAME = 'photoslibrary'
+API_VERSION = 'v1'
+CLIENT_SECRET_FILE = 'creds.json'
+SCOPES = [
+    'https://www.googleapis.com/auth/photoslibrary',
+    'https://www.googleapis.com/auth/photoslibrary.sharing',
+
+]
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_NAME, SCOPES)
